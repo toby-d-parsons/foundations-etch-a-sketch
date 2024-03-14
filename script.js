ae3bf8c9058c4513503354a5ec75a2205da1afe7
Toby Parsons <toby.d.parsons@gmail.com>
2024-03-14 22:54:17 +0000

    Add additional buttons
    Color/darken buttons, as well as clear option

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const body = document.body;
 const button = document.querySelector("#button-prompt");
 const divCont = document.createElement("div");
+const darkenBtn = document.querySelector("#darken-button");
+const colorBtn = document.querySelector("#color-button");
+const clearBtn = document.querySelector("#clear-button");
+darkenBtn.className = "button-selected";
 divCont.setAttribute("class", "grid-container");
 body.appendChild(divCont);
+let hoverType = 'darken';
+let numOfDivs = 16;
 
-function createGrid(dimension) {
+function createGrid(dimension, hoverType) {
     divCont.textContent = "";
     for (let i = 0; i < (dimension*dimension); i++) {
         let div = document.createElement("div");
@@ -12,28 +18,61 @@ function createGrid(dimension) {
         divCont.appendChild(div);
         //div.textContent = i+1;
         div.style.cssText = `flex-basis: ${1 / dimension * 100}%`;
+        let hoverCount = 0;
         div.addEventListener("mouseover", function() {
-            // div.classList.add("hover-item");
-            div.style.backgroundColor = "#"+randomColor();
+            if (hoverType == 'darken') {
+                while (hoverCount < 10) {
+                    hoverCount++;
+                    div.style.backgroundColor = `${darkenColor(hoverCount)}`;
+                    break;
+                }
+            } else {
+                div.style.backgroundColor = randomColor();
+            }
+            
         });
     }
 }
 
 function randomColor() {
-    let color = '';
-    while (color.length < 6) {
+    let color = '#';
+    while (color.length < 7) {
         color += Math.floor(Math.random() * 16).toString(16);
     } return color;
 }
 
+function darkenColor(hoverCount) {
+    let rgb = 255 - (25.5 * hoverCount); // Darkens by 10% each time
+    return 'rgb(' + rgb + ', ' + rgb + ', ' + rgb + ')';
+}
 
-createGrid(16);
+createGrid(numOfDivs, hoverType);
 
 button.addEventListener("click", () => {
-    let numOfDivs;
     do {
         numOfDivs = prompt("How many squares per side?", "16");
     } while (((numOfDivs > 0) && (numOfDivs < 101) && (Number.isInteger(Number(numOfDivs)))) != true);
-    console.log(numOfDivs);
-    createGrid(numOfDivs);
+    createGrid(numOfDivs, hoverType);
 });
+
+colorBtn.addEventListener("click", () => {
+    if (!colorBtn.classList.contains("button-selected")) {
+        hoverType = 'color';
+        colorBtn.classList = "button-selected";
+        darkenBtn.classList.remove("button-selected");
+        createGrid(numOfDivs, hoverType);
+    }
+})
+
+darkenBtn.addEventListener("click", () => {
+    if (!darkenBtn.classList.contains("button-selected")) {
+        hoverType = 'darken';
+        darkenBtn.classList = "button-selected";
+        colorBtn.classList.remove("button-selected");
+        createGrid(numOfDivs, hoverType);
+    }
+})
+
+clearBtn.addEventListener("click", () => {
+    createGrid(numOfDivs, hoverType);
+})
